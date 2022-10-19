cad8f4a66f453ded3e723ba18218dd6f9e005180
Aleks Panfilov <panfav40@gmail.com>
2022-10-20 00:43:48 +0300

    video added

--- RPIServer.py
+++ RPIServer.py
@@ -1,15 +1,21 @@
+import subprocess
+import multiprocessing
 import socket
 import time
 import pyttsx3
-import  serial
-import pyautogui
-import os
-import eel
+import serial
+# import os
+
+# Функция для воспроизведения видео в субпроцессе
 
 
-eel.init('./')
+def play(file_name):
+    active = multiprocessing.active_children()
+    for child in active:
+        child.terminate()
 
-eel.start('index.html')
+    subprocess.call(f'{file_name}', shell=True)
+
 
 baddr = 'B0:7D:64:DD:37:A8'
 channel = 1
@@ -22,11 +28,8 @@ ser = serial.Serial('/dev/ttyUSB0', 9600)
 ser.write(bytes('b', 'utf-8`'))
 pyttsx3.init()
 
-eel.change_img('default.gif')
-eel.start('index.html', mode='chrome', size=(800,480), position=(0,0), cmdline_args=['--start-fullscreen'])
-
-
 try:
+    play('animation.mp4')
     client, address = s.accept()
     while 1:
         data = client.recv(1024).decode('utf-8')
@@ -39,34 +42,26 @@ try:
         if data == 'joke':
             ser.write(bytes('s', 'utf-8'))
             pyttsx3.speak('Заходит улитка в бар...')
-            eel.change_img('glad.gif')
+            play('glad.mp4')
         if data == 'shark':
             pyttsx3.speak('Не заглядывал, страшно')
         if data == 'music':
-            eel.change_img('angry.gif')
-            eel.start('index.html', mode='chrome', size=(800, 480), position=(0, 0),
-                      cmdline_args=['--start-fullscreen'])
+            play('angry.mp4')
 
             pyttsx3.speak('Нет, я ужасный робот, и музыкальные вкусы у нас не сходятся')
             ser.write(bytes('b', 'utf-8'))
-            ser.write(bytes('q', 'utf-8')) # blink
+            ser.write(bytes('q', 'utf-8'))  # blink
         if data == '150':
-            eel.change_img('error.gif')
-            eel.start('index.html', mode='chrome', size=(800, 480), position=(0, 0),
-                      cmdline_args=['--start-fullscreen'])
+            play('error.mp4')
             pyttsx3.speak('150')
             time.sleep(3)
-            pyttsx3.speak('Слушайте.. Ну вот зачем Вам я? Купите лучше Эрика, он сможет сделать Вам еще хоть 1000 таких роботов.')
+            pyttsx3.speak('Слушайте.. Ну вот зачем Вам я? Купите лучше Эрика, он сможет сделать Вам еще хоть 1000 \
+            таких роботов.')
             time.sleep(3)
             pyttsx3.speak('Но Эрик же может сделать много роботов! И еда для Эрика дешевле электричества!')
         if data == 'yellow':
             ser.write(bytes('y', 'utf-8'))
-            eel.change_img('sad.gif')
-            eel.start('index.html', mode='chrome', size=(800, 480), position=(0, 0),
-                      cmdline_args=['--start-fullscreen'])
-
-
-
+            play('sad.mp4')
 
 except:
     print("Closing socket")
